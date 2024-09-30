3e872b91ca3610bae3a5492ca9885b61be836869
Mark Ian <markiantalan@gmail.com>
2024-09-30 19:10:31 +0800

    configure necessary dependencies

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<p>Hello world</p>
+</template>
+
+<script setup></script>
+
+<style lang="postcss" scoped>
+	p {
+		@apply bg-primary-500;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+import { store } from "@/store";
+
+import "@/assets/base.css";
+
+import App from "@/App.vue";
+
+const app = createApp(App);
+
+app.use(store);
+app.mount("#app");
